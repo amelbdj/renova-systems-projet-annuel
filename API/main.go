70efe5faf442b7ce13c57ca59e10b170ee39ca32
fatymut <158191632+fatymut@users.noreply.github.com>
2026-04-15 19:01:24 +0200

    login and inscription are done and the redirection is done

--- API/main.go
+++ API/main.go
@@ -33,13 +33,13 @@ func main() {
 
 	http.HandleFunc("OPTIONS /admin/categories/add", admin.CreateCategorie)
 	http.HandleFunc("OPTIONS /admin/categories/delete/{id}", admin.DeleteCategorie)
-	
+
 	http.HandleFunc("OPTIONS /admin/annonces/validate/{id}", admin.ValidateAnnonce)
 	http.HandleFunc("OPTIONS /admin/annonces/refuse/{id}", admin.RefuseAnnonce)
 	http.HandleFunc("OPTIONS /admin/annonces/add", auth.VerifyTokenMiddleware(admin.CreateAnnonce))
 	http.HandleFunc("OPTIONS /admin/annonces/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteAnnonce))
 	http.HandleFunc("OPTIONS /admin/annonces/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateAnnonce))
-	
+
 	http.HandleFunc("OPTIONS /admin/evenements/validate/{id}", admin.ValidateEvenement)
 	http.HandleFunc("OPTIONS /admin/evenements/refuse/{id}", admin.RefuseEvenement)
 
@@ -51,45 +51,45 @@ func main() {
 	http.HandleFunc("OPTIONS /admin/articles/validate/{id}", admin.ValidateArticle)
 	http.HandleFunc("OPTIONS /admin/articles/refuse/{id}", admin.RefuseArticle)
 	http.HandleFunc("OPTIONS /admin/articles/delete/{id}", admin.DeleteArticle)
-http.HandleFunc("OPTIONS /admin/articles/add/{action}", admin.CreateArticle)
-http.HandleFunc("OPTIONS /admin/articles/modify/{id}/{action}", admin.ModifyArticle)
+	http.HandleFunc("OPTIONS /admin/articles/add/{action}", admin.CreateArticle)
+	http.HandleFunc("OPTIONS /admin/articles/modify/{id}/{action}", admin.ModifyArticle)
 
 	// Routes protégées par le middleware d'authentification
 	// Note : Le middleware doit être appliqué à chaque route qui nécessite une authentification
 
-
 	// Users
-    http.HandleFunc("GET /admin/users", admin.GetAllUsers)
-    http.HandleFunc("POST /admin/users/add", auth.VerifyTokenMiddleware(admin.CreateUser))
-    http.HandleFunc("DELETE /admin/users/delete/{id}", auth.VerifyTokenMiddleware(admin.DeletedUser))
-    http.HandleFunc("PUT /admin/users/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateUser))
-    http.HandleFunc("GET /admin/users/role/{role}", auth.VerifyTokenMiddleware(admin.GetUserByRole))
-    http.HandleFunc("GET /admin/users/search", auth.VerifyTokenMiddleware(admin.GetUserByName))
+	http.HandleFunc("GET /admin/users", admin.GetAllUsers)
+	http.HandleFunc("POST /admin/users/add", auth.VerifyTokenMiddleware(admin.CreateUser))
+	http.HandleFunc("DELETE /admin/users/delete/{id}", auth.VerifyTokenMiddleware(admin.DeletedUser))
+	http.HandleFunc("PUT /admin/users/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateUser))
+	http.HandleFunc("GET /admin/users/role/{role}", auth.VerifyTokenMiddleware(admin.GetUserByRole))
+	http.HandleFunc("GET /admin/users/search", auth.VerifyTokenMiddleware(admin.GetUserByName))
 	http.HandleFunc("PUT /admin/users/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateUser))
-	http.HandleFunc("PUT /admin/users/refuse/{id}",auth.VerifyTokenMiddleware(admin.RefuseUser))
+	http.HandleFunc("PUT /admin/users/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseUser))
 	http.HandleFunc("/api/upload-document", auth.VerifyTokenMiddleware(admin.UploadDocumentHandler))
 	http.Handle("/view-uploads/", http.StripPrefix("/view-uploads/", http.FileServer(http.Dir("./uploads"))))
+	http.HandleFunc("/auth/check-email", admin.VerifierEmail)
+	http.HandleFunc("/auth/inscription", admin.Inscription)
+	http.HandleFunc("/admin/login", admin.Login)
 
+	// Categories
+	http.HandleFunc("POST /admin/categories/add", auth.VerifyTokenMiddleware(admin.CreateCategorie))
+	http.HandleFunc("DELETE /admin/categories/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteCategorie))
+	http.HandleFunc("GET /admin/categories", auth.VerifyTokenMiddleware(admin.GetAllCategories))
 
-    // Categories
-    http.HandleFunc("POST /admin/categories/add", auth.VerifyTokenMiddleware(admin.CreateCategorie))
-    http.HandleFunc("DELETE /admin/categories/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteCategorie))
-    http.HandleFunc("GET /admin/categories", auth.VerifyTokenMiddleware(admin.GetAllCategories))
-
-    // Annonces
-    http.HandleFunc("GET /admin/annonces", admin.GetAllAnnonces)
-    http.HandleFunc("PUT /admin/annonces/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateAnnonce))
-    http.HandleFunc("PUT /admin/annonces/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseAnnonce))
+	// Annonces
+	http.HandleFunc("GET /admin/annonces", admin.GetAllAnnonces)
+	http.HandleFunc("PUT /admin/annonces/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateAnnonce))
+	http.HandleFunc("PUT /admin/annonces/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseAnnonce))
 	http.HandleFunc("POST /admin/annonces/add", auth.VerifyTokenMiddleware(admin.CreateAnnonce))
 	http.HandleFunc("DELETE /admin/annonces/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteAnnonce))
 	http.HandleFunc("PUT /admin/annonces/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateAnnonce))
 	http.HandleFunc("GET /admin/annonces/search", auth.VerifyTokenMiddleware(admin.GetAnnonceByTitle))
 
-
-    // Evenements
-    http.HandleFunc("GET /admin/evenements", auth.VerifyTokenMiddleware(admin.GetAllEvenements))
-    http.HandleFunc("PUT /admin/evenements/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateEvenement))
-    http.HandleFunc("PUT /admin/evenements/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseEvenement))
+	// Evenements
+	http.HandleFunc("GET /admin/evenements", auth.VerifyTokenMiddleware(admin.GetAllEvenements))
+	http.HandleFunc("PUT /admin/evenements/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateEvenement))
+	http.HandleFunc("PUT /admin/evenements/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseEvenement))
 
 	// Traductions
 	http.HandleFunc("GET /api/translations", admin.GetTranslations)
@@ -118,5 +118,4 @@ http.HandleFunc("OPTIONS /admin/articles/modify/{id}/{action}", admin.ModifyArti
 	fmt.Println("test de : http://localhost:8081")
 	http.ListenAndServe(":8081", nil)
 
-	
 }
