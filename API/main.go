67718af9471dd3ca9f0d3f42fc1de12b52070be4
fatymut <158191632+fatymut@users.noreply.github.com>
2026-04-16 23:12:48 +0200

    resolved conflict we had and made sure it worked

--- API/main.go
+++ API/main.go
@@ -45,7 +45,7 @@ func main() {
 	http.HandleFunc("OPTIONS /admin/evenements/delete/{id}", admin.DeleteEvenement)
 	http.HandleFunc("OPTIONS /admin/evenements/refuse/{id}", admin.RefuseEvenement)
 	http.HandleFunc("OPTIONS /admin/evenements/add", admin.CreateEvenement)
-    http.HandleFunc("OPTIONS /admin/evenements",admin.GetAllEvenements)
+	http.HandleFunc("OPTIONS /admin/evenements", admin.GetAllEvenements)
 
 	http.HandleFunc("OPTIONS /admin/box/confirm-deposit", admin.ConfirmDeposit)
 	http.HandleFunc("OPTIONS /admin/box/collect-object", admin.CollectObject)
@@ -57,34 +57,31 @@ func main() {
 	http.HandleFunc("OPTIONS /admin/articles/delete/{id}", admin.DeleteArticle)
 	http.HandleFunc("OPTIONS /admin/articles/add/{action}", admin.CreateArticle)
 	http.HandleFunc("OPTIONS /admin/articles/modify/{id}/{action}", admin.ModifyArticle)
-    http.HandleFunc("OPTIONS /admin/articles/salarie/{id}", admin.GetArticlesBySalarie)
-
+	http.HandleFunc("OPTIONS /admin/articles/salarie/{id}", admin.GetArticlesBySalarie)
 
 	// --- USERS ---
-    http.HandleFunc("GET /admin/users", auth.VerifyTokenMiddleware(admin.GetAllUsers)) 
-    http.HandleFunc("POST /admin/users/add", auth.VerifyTokenMiddleware(admin.CreateUser))
-    http.HandleFunc("DELETE /admin/users/delete/{id}", auth.VerifyTokenMiddleware(admin.DeletedUser))
-    http.HandleFunc("PUT /admin/users/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateUser))
-    http.HandleFunc("GET /admin/users/role/{role}", auth.VerifyTokenMiddleware(admin.GetUserByRole))
-    http.HandleFunc("GET /admin/users/search", auth.VerifyTokenMiddleware(admin.GetUserByName))
-    http.HandleFunc("PUT /admin/users/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateUser))
-    http.HandleFunc("PUT /admin/users/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseUser))
-    
-    // Auth, Tutorial & Upload
-    http.HandleFunc("/api/upload-document", auth.VerifyTokenMiddleware(admin.UploadDocumentHandler))
-    http.Handle("/view-uploads/", http.StripPrefix("/view-uploads/", http.FileServer(http.Dir("./uploads"))))
-    http.HandleFunc("/auth/check-email", admin.VerifierEmail) 
-    http.HandleFunc("/auth/inscription", admin.Inscription)   
-    http.HandleFunc("/admin/login", admin.Login)
-    http.HandleFunc("/update-tutorial", admin.UpdateTutorialStatus)
+	http.HandleFunc("GET /admin/users", auth.VerifyTokenMiddleware(admin.GetAllUsers))
+	http.HandleFunc("POST /admin/users/add", auth.VerifyTokenMiddleware(admin.CreateUser))
+	http.HandleFunc("DELETE /admin/users/delete/{id}", auth.VerifyTokenMiddleware(admin.DeletedUser))
+	http.HandleFunc("PUT /admin/users/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateUser))
+	http.HandleFunc("GET /admin/users/role/{role}", auth.VerifyTokenMiddleware(admin.GetUserByRole))
+	http.HandleFunc("GET /admin/users/search", auth.VerifyTokenMiddleware(admin.GetUserByName))
+	http.HandleFunc("PUT /admin/users/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateUser))
+	http.HandleFunc("PUT /admin/users/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseUser))
 
-    // --- CATEGORIES ---
-    http.HandleFunc("POST /admin/categories/add", auth.VerifyTokenMiddleware(admin.CreateCategorie))
-    http.HandleFunc("DELETE /admin/categories/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteCategorie))
-    http.HandleFunc("GET /admin/categories", auth.VerifyTokenMiddleware(admin.GetAllCategories))
+	// Auth, Tutorial & Upload
+	http.HandleFunc("/api/upload-document", auth.VerifyTokenMiddleware(admin.UploadDocumentHandler))
+	http.Handle("/view-uploads/", http.StripPrefix("/view-uploads/", http.FileServer(http.Dir("./uploads"))))
+	http.HandleFunc("/auth/check-email", admin.VerifierEmail)
+	http.HandleFunc("/auth/inscription", admin.Inscription)
+	http.HandleFunc("/admin/login", admin.Login)
+	http.HandleFunc("/update-tutorial", admin.UpdateTutorialStatus)
 
-<<<<<<< HEAD
-	// Annonces
+	// --- CATEGORIES ---
+	http.HandleFunc("POST /admin/categories/add", auth.VerifyTokenMiddleware(admin.CreateCategorie))
+	http.HandleFunc("DELETE /admin/categories/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteCategorie))
+	http.HandleFunc("GET /admin/categories", auth.VerifyTokenMiddleware(admin.GetAllCategories))
+	// --- ANNONCES ---
 	http.HandleFunc("GET /admin/annonces", admin.GetAllAnnonces)
 	http.HandleFunc("PUT /admin/annonces/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateAnnonce))
 	http.HandleFunc("PUT /admin/annonces/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseAnnonce))
@@ -93,46 +90,33 @@ func main() {
 	http.HandleFunc("PUT /admin/annonces/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateAnnonce))
 	http.HandleFunc("GET /admin/annonces/search", auth.VerifyTokenMiddleware(admin.GetAnnonceByTitle))
 	http.HandleFunc("GET /mes-annonces", admin.GetMyAnnonces)
-	// Evenements
-	http.HandleFunc("GET /admin/evenements", auth.VerifyTokenMiddleware(admin.GetAllEvenements))
+
+	// --- EVENEMENTS ---
+	http.HandleFunc("GET /admin/evenements", admin.GetAllEvenements)
+	http.HandleFunc("POST /admin/evenements/add", auth.VerifyTokenMiddleware(admin.CreateEvenement))
+	http.HandleFunc("PUT /admin/evenements/{id}", auth.VerifyTokenMiddleware(admin.UpdateEvenement))
+	http.HandleFunc("DELETE /admin/evenements/{id}", auth.VerifyTokenMiddleware(admin.DeleteEvenement))
 	http.HandleFunc("PUT /admin/evenements/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateEvenement))
 	http.HandleFunc("PUT /admin/evenements/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseEvenement))
-=======
-    // --- ANNONCES ---
-    http.HandleFunc("GET /admin/annonces", admin.GetAllAnnonces) // Peut-être public ?
-    http.HandleFunc("PUT /admin/annonces/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateAnnonce))
-    http.HandleFunc("PUT /admin/annonces/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseAnnonce))
-    http.HandleFunc("POST /admin/annonces/add", auth.VerifyTokenMiddleware(admin.CreateAnnonce))
-    http.HandleFunc("DELETE /admin/annonces/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteAnnonce))
-    http.HandleFunc("PUT /admin/annonces/modify/{id}", auth.VerifyTokenMiddleware(admin.UpdateAnnonce))
 
-    // --- EVENEMENTS ---
-    http.HandleFunc("GET /admin/evenements", admin.GetAllEvenements) 
-    http.HandleFunc("POST /admin/evenements/add", auth.VerifyTokenMiddleware(admin.CreateEvenement)) // AJOUTÉ
-    http.HandleFunc("PUT /admin/evenements/{id}", auth.VerifyTokenMiddleware(admin.UpdateEvenement)) // AJOUTÉ
-    http.HandleFunc("DELETE /admin/evenements/{id}", auth.VerifyTokenMiddleware(admin.DeleteEvenement)) // AJOUTÉ
-    http.HandleFunc("PUT /admin/evenements/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateEvenement))
-    http.HandleFunc("PUT /admin/evenements/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseEvenement))
->>>>>>> 65ebd7667305997513db50c808e83e63bb0187ee
+	// --- LOGISTIQUE ---
+	http.HandleFunc("POST /admin/orders/create", auth.VerifyTokenMiddleware(admin.CreateOrder))
+	http.HandleFunc("POST /admin/box/confirm-deposit", auth.VerifyTokenMiddleware(admin.ConfirmDeposit))
+	http.HandleFunc("POST /admin/box/collect-object", auth.VerifyTokenMiddleware(admin.CollectObject))
+	http.HandleFunc("GET /admin/boxs", auth.VerifyTokenMiddleware(admin.GetAllBoxs))       // AJOUTÉ
+	http.HandleFunc("POST /admin/box/create", auth.VerifyTokenMiddleware(admin.CreateBox)) // AJOUTÉ
 
-    // --- LOGISTIQUE ---
-    http.HandleFunc("POST /admin/orders/create", auth.VerifyTokenMiddleware(admin.CreateOrder))
-    http.HandleFunc("POST /admin/box/confirm-deposit", auth.VerifyTokenMiddleware(admin.ConfirmDeposit))
-    http.HandleFunc("POST /admin/box/collect-object", auth.VerifyTokenMiddleware(admin.CollectObject))
-    http.HandleFunc("GET /admin/boxs", auth.VerifyTokenMiddleware(admin.GetAllBoxs)) // AJOUTÉ
-    http.HandleFunc("POST /admin/box/create", auth.VerifyTokenMiddleware(admin.CreateBox)) // AJOUTÉ
+	// --- ARTICLES / NEWS ---
+	http.HandleFunc("GET /admin/articles", admin.GetAllArticles)
+	http.HandleFunc("GET /admin/articles/salarie/{id}", auth.VerifyTokenMiddleware(admin.GetArticlesBySalarie))
+	http.HandleFunc("GET /admin/articles/{id}", admin.GetArticleById)                                       // AJOUTÉ
+	http.HandleFunc("PUT /admin/articles/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateArticle)) // AJOUTÉ
+	http.HandleFunc("PUT /admin/articles/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseArticle))
+	http.HandleFunc("DELETE /admin/articles/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteArticle))       // AJOUTÉ
+	http.HandleFunc("POST /admin/articles/add/{action}", auth.VerifyTokenMiddleware(admin.CreateArticle))        // AJOUTÉ
+	http.HandleFunc("PUT /admin/articles/modify/{id}/{action}", auth.VerifyTokenMiddleware(admin.ModifyArticle)) // AJOUTÉ
 
-    // --- ARTICLES / NEWS ---
-    http.HandleFunc("GET /admin/articles", admin.GetAllArticles)
-    http.HandleFunc("GET /admin/articles/salarie/{id}", auth.VerifyTokenMiddleware(admin.GetArticlesBySalarie))
-    http.HandleFunc("GET /admin/articles/{id}", admin.GetArticleById) // AJOUTÉ
-    http.HandleFunc("PUT /admin/articles/validate/{id}", auth.VerifyTokenMiddleware(admin.ValidateArticle)) // AJOUTÉ
-    http.HandleFunc("PUT /admin/articles/refuse/{id}", auth.VerifyTokenMiddleware(admin.RefuseArticle))
-    http.HandleFunc("DELETE /admin/articles/delete/{id}", auth.VerifyTokenMiddleware(admin.DeleteArticle)) // AJOUTÉ
-    http.HandleFunc("POST /admin/articles/add/{action}", auth.VerifyTokenMiddleware(admin.CreateArticle)) // AJOUTÉ
-    http.HandleFunc("PUT /admin/articles/modify/{id}/{action}", auth.VerifyTokenMiddleware(admin.ModifyArticle)) // AJOUTÉ
+	fmt.Println("test de : http://localhost:8081")
 
-    fmt.Println("test de : http://localhost:8081")
- 
-    http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8081", nil)
 }
