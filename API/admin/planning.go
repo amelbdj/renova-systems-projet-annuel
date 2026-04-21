698bf4adffbec9e78df842a63813e7e618eca493
amelbdj <amelboudjenanee@icloud.com>
2026-04-21 18:28:44 +0200

    planning user

---
+++ API/admin/planning.go
@@ -0,0 +1,45 @@
+package admin
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"upcycleconnect/bdd"
+)
+
+func GetUserPlanningHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+
+	idStr := r.URL.Query().Get("id") 
+	if idStr == "" {
+		http.Error(w, "ID utilisateur manquant", http.StatusBadRequest)
+		return
+	}
+
+	userID, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "ID utilisateur invalide", http.StatusBadRequest)
+		return
+	}
+
+
+	planningData, err := bdd.GetUserPlanning(userID)
+	if err != nil {
+		http.Error(w, "Erreur lors du chargement du planning", http.StatusInternalServerError)
+		fmt.Println("Erreur GetUserPlanningHandler:", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(planningData)
+}
