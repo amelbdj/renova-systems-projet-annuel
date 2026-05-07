ebe2f383cc952b973fc2c1e543e891590bd07840
amelbdj <amelboudjenanee@icloud.com>
2026-05-07 23:50:54 +0200

    refontye du dash admin totale

--- API/bdd/annonceReq.go
+++ API/bdd/annonceReq.go
@@ -10,7 +10,7 @@ func GetAnnonces() ([]models.Annonce, error) {
 
 	var Annonces []models.Annonce
 
-	rows, err := Db.Query("SELECT a.id, a.titre, a.description, a.type, a.prix, a.statut_validation, a.code_postal, a.ville, a.etat, a.poids_kg, a.quantite, a.image, u.nom, u.prenom, c.libelle FROM pa2026.annonce a INNER JOIN pa2026.utilisateur u ON u.id = a.id_user INNER JOIN pa2026.categorie c ON c.id = a.id_categorie")
+	rows, err := Db.Query("SELECT DATE_FORMAT(a.created_at, '%d-%m-%Y') as created_at, a.id, a.titre, a.description, a.type, a.prix, a.statut_validation, a.code_postal, a.ville, a.etat, a.poids_kg, a.quantite, a.image, u.nom, u.prenom, c.libelle FROM pa2026.annonce a INNER JOIN pa2026.utilisateur u ON u.id = a.id_user INNER JOIN pa2026.categorie c ON c.id = a.id_categorie")
 
 	if err != nil {
 		return nil, fmt.Errorf("get Annonces : %v", err.Error())
@@ -21,7 +21,7 @@ func GetAnnonces() ([]models.Annonce, error) {
 
 		var Annonce models.Annonce
 
-		err := rows.Scan(&Annonce.Id, &Annonce.Titre, &Annonce.Description, &Annonce.Type, &Annonce.Prix, &Annonce.StatutValidation,
+		err := rows.Scan(&Annonce.CreatedAt, &Annonce.Id, &Annonce.Titre, &Annonce.Description, &Annonce.Type, &Annonce.Prix, &Annonce.StatutValidation,
 			&Annonce.CodePostal, &Annonce.Ville, &Annonce.Etat, &Annonce.PoidsKg, &Annonce.Quantite, &Annonce.Image,
 			&Annonce.Nom, &Annonce.Prenom,
 			&Annonce.Categorie)
